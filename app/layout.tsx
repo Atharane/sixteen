5404c2bb23176e53037abcaa623a0d5950ecdc48
atharane <atharva@otherwise.company>
2025-10-26 19:03:50 +0530

    feat(globals.css): enhance global styles with new CSS variables for better theming support feat(layout.tsx): integrate ThemeProvider for theme management and dark mode support feat(components.json): add configuration file for component library settings feat(utils.ts): create utility function for merging class names with Tailwind CSS chore(package.json): update dependencies and add new packages for theming and utility functions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,12 +25,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </ThemeProvider>
   );
 }
